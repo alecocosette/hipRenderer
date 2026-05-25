7732f42f51721ff834a5857eb30bc5d6604ac3f5
Alejandro <alecoco09j@gmail.com>
2026-05-25 18:15:09 -0400

    first image with ppm :)

--- gurt.cpp
+++ gurt.cpp
@@ -4,6 +4,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <iostream>
 int main() {
-  printf("Hello World!\n");
+    int image_width = 256;
+    int image_height = 256;
+    std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
+    for (int i = 0; i < image_width; i++){
+        for (int j = 0; j < image_height; j++) {
+            auto r = double(i)/(image_width-1);
+            auto g = double(j)/(image_height-1);
+            auto b = 0.0;
+            int ir = int(255.99*r);
+            int ig = int(255.99*g);
+            int ib = int(255.99*b);
+            std::cout << ir << " " << ig << " " << ib << "\n";
+        }
+    }
+
 }
